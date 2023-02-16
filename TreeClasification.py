244e050d8e0013a8e53957e6b8d16175c2e5937f
Andrei Lupica <64814779+clemaitoro@users.noreply.github.com>
2023-02-16 20:09:35 +0100

    Add files via upload

---
+++ TreeClasification.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# -------------------------- Data Processing ---------------------------------
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+
+x = dataset.iloc[:, :-1].values
+
+y = dataset.iloc[:, -1].values
+
+
+# -------------------------- Data Training ---------------------------------
+from sklearn.model_selection import train_test_split
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+# -------------------------- Data scaling -----------------------------------
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
+
+# --------------------------- Training the model ---------------------------
+from sklearn.tree import DecisionTreeClassifier
+
+regressor = DecisionTreeClassifier(random_state=0, criterion='entropy')
+
+regressor.fit(x_train, y_train)
+
+
+#--------------------------- Predicting the results -----------------------
+# print(regressor.predict(x_test[[0]]))
+# print(regressor.predict(sc.transform([[30, 87000]])))
+
+
+y_pred = regressor.predict(x_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+
+# ------------------------ Accuracy and confusion metrics ----------------
+from sklearn.metrics import accuracy_score,confusion_matrix
+
+accuracy = accuracy_score(y_test, y_pred)
+matrix = confusion_matrix(y_test, y_pred)
+
+print(matrix)
+print(accuracy)
+
+# --------------------------- Visualising  results ----------------------
+# from matplotlib.colors import ListedColormap
+# X_set, y_set = sc.inverse_transform(x_train), y_train
+# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+#                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+# plt.contourf(X1, X2, regressor.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+#              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+# plt.title('Logistic Regression (Training set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
+
+
+# Visualising the Test set results
+# from matplotlib.colors import ListedColormap
+# X_set, y_set = sc.inverse_transform(x_test), y_test
+# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+#                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+# plt.contourf(X1, X2, regressor.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+#              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+# plt.title('Logistic Regression (Test set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
+
