a49d6eafd3a90a8982c1322bf7d50a8d7dbf7458
Andrei Lupica <64814779+clemaitoro@users.noreply.github.com>
2023-02-21 13:38:41 +0100

    Association Learning models

---
+++ Apriori.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# --------------------- Importing the data -----------------------------
+
+dataset = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
+
+transactions = []
+
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+
+# --------------------- Training the model -----------------------------
+from apyori import apriori
+
+rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2,
+                min_lift=3, min_length=2, max_length=2)
+
+
+# --------------------- Visualising results ---------------------------
+
+# Displaying the first results from the function
+results = list(rules)
+# print(results)
+
+
+# Putting the results well organised into a Pandas Dataframe
+def inspect(results):
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side',
+                                                               'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+
+
+# Displaying the results non sorted
+# print(resultsinDataFrame)
+
+
+# Displaying sorted resutls
+resultsinDataFrame.nlargest(n=10, columns="lift")
